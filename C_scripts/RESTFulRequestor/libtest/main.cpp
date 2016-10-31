a2630c7153f139557ea496a5bcbd83f366f22e90
hellmonky <hellmonky@qq.com>
2016-10-31 15:12:15 +0800

    添加复杂struct嵌套测试

--- C_scripts/RESTFulRequestor/libtest/main.cpp
+++ C_scripts/RESTFulRequestor/libtest/main.cpp
@@ -1,9 +1,9 @@
 // RESTFul API 访问接口
 #include "RESTFulRequestor.cpp"
 
+/*********************************************************************/
 // 测试结构体的序列化和反序列化
-struct Person
-{
+struct Person {
 	int age;
 	string name;
 	string city;
@@ -12,6 +12,127 @@ struct Person
 	META(age, name, city)
 };
 Person p = { 18, "bb", "aa" };
+
+/*********************************************************************/
+// 复杂嵌套结构体转换测试
+struct Pos_OUT
+{
+	float lat;//纬度
+	float lon;//经度
+	float atl;//高度，可能没有
+
+	// metaInfo
+	META(lat, lon, atl)
+};
+struct Action_OUT
+{
+	string id;//实体id，不一定有
+	string name;//路线名，或者地点名，XXX县
+	string code;//路径或地点编号
+	string startTime;//开始时间
+	string stopTime;//到达时间
+	vector<Pos_OUT> posList;//途经位置点，如果为地点，即阶段2，则只有一项
+
+	// metaInfo
+	META(id, name, code, startTime, stopTime, posList)
+};
+struct Object_OUT
+{
+	string id;
+	string name;//名称
+	string code;//编号
+	int  type;//类型
+
+	// metaInfo
+	META(id, name, code, type)
+};
+struct Plan_OUT
+{
+	string name;//方案名称
+	string startTime;//开始时间
+	string stopTime;//到达时间
+	int stage;//阶段
+	vector<Object_OUT> objList;//行动目标序列
+	vector<Action_OUT> rounteList;//行动序列
+
+	// metaInfo
+	META(name, startTime, stopTime, stage, objList, rounteList)
+};
+struct DataContent {
+	int type;
+	vector<Plan_OUT> command;
+
+	// metaInfo
+	META(type, command)
+};
+struct Content {
+	string timestamp;
+	vector<DataContent> data;
+
+	// metaInfo
+	META(timestamp, data)
+};
+struct Mission {
+	string from;
+	string to;
+	int type;
+	Content content;
+
+	// metaInfo
+	META(from, to, type, content)
+};
+
+/*********************************************************************/
+// 测试转换函数：
+void complexStrcutTest() {
+	//
+	Pos_OUT post_out_1 = { double(18.9), double(20.5), double(0) };
+	Pos_OUT post_out_2 = { float(19.9), float(21.5), float(0) };
+	Pos_OUT post_out_3 = { float(20.9), float(22.5), float(0) };
+	vector<Pos_OUT> posList;
+	//
+	posList.push_back(post_out_1);
+	posList.push_back(post_out_2);
+	posList.push_back(post_out_3);
+	//
+	Action_OUT action_out = { "1", "国道333", "001", "20161019100000", "20161019110000", posList };
+	//
+	Object_OUT object_out_1 = { "1", "目标1", "001", 1 };
+	Object_OUT object_out_2 = { "2", "目标2", "002", 1 };
+	//
+	vector<Object_OUT> objList;
+	objList.push_back(object_out_1);
+	objList.push_back(object_out_2);
+	vector<Action_OUT> rounteList;
+	rounteList.push_back(action_out);
+	Plan_OUT plan_out = { "任务1阶段1", "20161019080000", "20161021080000", 1, objList, rounteList };
+	//
+	vector<Plan_OUT> command;
+	command.push_back(plan_out);
+	DataContent datacontent = { 1, command };
+	//
+	vector<DataContent> data;
+	data.push_back(datacontent);
+	Content content = { "20161019103030", data };
+	//
+	Mission mission = { "sourceId", "targetId", 1, content };
+
+
+	// 开始序列化为JSON：
+	RESTFulRequestor<Mission> restfulK;
+	// 测试：从自定义结构体转换为JSON
+	string serializitionResult = restfulK.serialize(mission);
+	cout << "转换后的JSON结果为：" << serializitionResult << endl;
+	// 测试：从JSON转换到自定义结构体
+	Mission deserializitionResult;
+	bool result = restfulK.deserialize(deserializitionResult, serializitionResult);
+	if (result) {
+		cout << "反序列化成功！" << endl;
+	}
+}
+
+
+
 
 // 字符集转换函数
 std::string utf8_from_iso8859_1(std::string str)
@@ -27,7 +148,6 @@ std::string utf8_from_iso8859_1(std::string str)
 	}
 	return res;
 }
-
 string GBKToUTF8(const std::string & strGBK) {
 	string strOutUTF8 = "";
 	WCHAR * str1;
@@ -65,6 +185,9 @@ string UTF8ToGBK(const std::string & strUTF8)
 // 测试函数入口：
 int main(int argc, char * argv[]) {
 
+	//
+	complexStrcutTest();
+
 	// 初始化restful请求类的实例
 	RESTFulRequestor<Person> request;
 
@@ -80,7 +203,7 @@ int main(int argc, char * argv[]) {
 	string PutURL = "http://172.16.10.14:1569/datasong/dataService/iscas/mission";
 	string PutParamters = "{\"_actionid\" : \"1\", \"_id\" : \"\",  \"_name\" : \"任务1\",  \"_remark\" : \"我的第一个任务\",  \"_time\" : 2.119999999999999}";
 	cout << PutParamters << endl;
-	string PutResult = request.testPUTRequest2Stream(PutURL, GBKToUTF8(PutParamters));
+	string PutResult = request.getPUTRequest2Stream(PutURL, GBKToUTF8(PutParamters));
 	cout << UTF8ToGBK(PutResult) << endl;
 	//string PutFileName = "C:\\Users\\wentao\\Desktop\\out.txt";
 	//bool isPUTSuccess = request.getPUTRequestAsFile(GetChineseURL, PutParamters, PutFileName);
