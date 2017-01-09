b9a58247423975b4bdde00b2907c679dbae5b2e6
WENTAO-PC\文涛 <hellmonky@qq.com>
2017-01-09 15:26:35 +0800

    完成调试，发布第一版

--- shell/installHadoopCluster/installHadoopCluster.sh
+++ shell/installHadoopCluster/installHadoopCluster.sh
@@ -52,8 +52,7 @@ function installJDK(){
     destfilepath=$3
     destfolderename=$4
     eval "tar -zxf ${tarfilepath} -C ${destfilepath}"
-    cd ${destfilepath}
-    mv ${tarfoldername}/ ${destfolderename}
+    mv ${destfilepath}/${tarfoldername}/ ${destfilepath}/${destfolderename}
     sysintfile="/etc/profile"
     echo "export JAVA_HOME=${destfilepath}/${destfolderename}" >> ${sysintfile}
     echo "export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH" >> ${sysintfile}
@@ -66,25 +65,21 @@ function installMySQL(){
     filename=$3
     cnfpath=$4
     tar -zxf ${tarfilepath} -C ${desfilepath}
-    cd ${desfilepath}
-    mv ${filename}  ./mysql
+    mv ${desfilepath}/${filename} ${desfilepath}/mysql
     groupadd mysql
     useradd -r -g mysql mysql
-    chown -R mysql:mysql ./mysql
-    ./mysql/bin/mysqld --initialize-insecure --user=mysql  
+    chown -R mysql:mysql ${desfilepath}/mysql
+    ${desfilepath}/mysql/bin/mysqld --initialize-insecure --user=mysql  
 	
     cp  ${cnfpath} /etc/my.cnf 
 }
 function startMySQL(){
-     cd /usr/local/mysql/support-files/
-	 ./mysql.server start
+	 /usr/local/mysql/support-files/mysql.server start
 }
 function stopMySQL(){
-     cd /usr/local/mysql/support-files/
-	 ./mysql.server stop
+	 /usr/local/mysql/support-files/mysql.server stop
 }
 function addRomoteLoginUser(){
-    cd /usr/local/mysql/bin/
     mysqllogin="root"
     mysqlpass="" 
     newusername="iscas"
@@ -93,26 +88,23 @@ function addRomoteLoginUser(){
     sql_grant1="GRANT ALL PRIVILEGES ON *.* TO '${newusername}'@'%';";
     sql_grant2="GRANT ALL PRIVILEGES ON *.* TO '${newusername}'@'localhost';";
     sql_add="${sql_createuser}${sql_grant1}${sql_grant2}";
-    ./mysql --user=$mysqllogin --password=$mysqlpass --execute="$sql_add";
+    /usr/local/mysql/bin/mysql --user=$mysqllogin --password=$mysqlpass --execute="$sql_add";
 }
 function deleteuser(){
-    cd /usr/local/mysql/bin/
     delusername="iscas"
     mysqllogin="root" 
     mysqlpass="" 
     sql_del1="DROP user '${delusername}'@'%';";
     sql_del2="DROP user '${delusername}'@localhost;";
     sql_add="${sql_del1}";
-   ./mysql --user=$mysqllogin --password=$mysqlpass --execute="$sql_add";
+    /usr/local/mysql/bin/mysql --user=$mysqllogin --password=$mysqlpass --execute="$sql_add";
 }
 function rootUserLogin(){
-    cd /usr/local/mysql/bin/
     mysqllogin="root"
     mysqlpass=""
-   ./mysql --user=$mysqllogin --password=$mysqlpass ;
+    /usr/local/mysql/bin/mysql --user=$mysqllogin --password=$mysqlpass ;
 }
 function createtable(){
-    cd /usr/local/mysql/bin/
     mysqllogin="iscas" 
     mysqlpass="12345" 
     createdb="test";   
@@ -122,22 +114,20 @@ function createtable(){
     select_db="use ${createdb};";
     sql_loc="source ${location};";
     sql_add="${sql_db}${select_db}${sql_loc}";
-   ./mysql --user=$mysqllogin --password=$mysqlpass  -h$ipaddr --execute="$sql_add";
+    /usr/local/mysql/bin/mysql --user=$mysqllogin --password=$mysqlpass  -h$ipaddr --execute="$sql_add";
 }
 function localhostlogin(){
-    cd /usr/local/mysql/bin/
     ipaddr=`ip route show | sed -n '2'p|awk '{print $9}'`
     mysqllogin="iscas"
     mysqlpass="12345"
-   ./mysql --user=$mysqllogin --password=$mysqlpass -h$ipaddr;
+    /usr/local/mysql/bin/mysql --user=$mysqllogin --password=$mysqlpass -h$ipaddr;
 }
 function releaseES(){
-    es_tarpath='/usr/local/elasticsearch-1.6.0.tar.gz'
+    es_tarpath='/usr/local/package/elasticsearch-1.6.0.tar.gz'
     desfilepath='/usr/local/'   
     foldername='elasticsearch-1.6.0'
     releaseTarPackage ${es_tarpath} ${desfilepath}
-    cd  ${desfilepath}
-    chomd -R 775 ${desfilepath}/${foldername}
+    chmod -R 775 ${desfilepath}/${foldername}
 }
 function updateES(){
    nodename=$1
@@ -151,25 +141,23 @@ function installES(){
     releaseES
     updateES ${currentHostname}
 }
-function startES(){
-     cd    /usr/local
-    ./elasticsearch-1.6.0/bin/elasticsearch  -d
+function startES(){  
+    /usr/local/elasticsearch-1.6.0/bin/elasticsearch  -d
 }
 function stopES(){
     ps -ef | grep elasticsearch | awk '{print $2}'|xargs kill 
 }
 function installfilebeat(){
-    fb_tarpath='/usr/local/filebeat-1.0.0-x86_64.tar.gz'
+    fb_tarpath='/usr/local/package/filebeat-1.0.0-x86_64.tar.gz'
     desfilepath='/usr/local/'
     tar -zvxf ${fb_tarpath} -C ${desfilepath}
 }
 function updatefilebeat(){
     currentHostname=$1
     currentIP=$2
-   fb_path='/usr/local/filebeat.yml'
+   fb_path='/usr/local/package/filebeat.yml'
    cp ${fb_path}  /usr/local/filebeat-1.0.0-x86_64/
-   cd /usr/local/filebeat-1.0.0-x86_64/
-   FILE=./filebeat.yml
+   FILE=/usr/local/filebeat-1.0.0-x86_64/filebeat.yml
    nodename=${currentHostname}
    nodeip=${currentIP}
    KEY1='#name:'
@@ -180,50 +168,45 @@ function updatefilebeat(){
    sed -i  's/'"$KEY2"'/'"$INSTEAD2"'/g'  $FILE
 }
 function startfilebeat(){
-    cd /usr/local/
-    ./filebeat-1.0.0-x86_64/filebeat -e -c ./filebeat.yml   &
+    /usr/local/filebeat-1.0.0-x86_64/filebeat -e -c ./filebeat.yml   &
 }
 function stopfilebeat(){
    ps -ef | grep filebeat | awk '{print $2}'|xargs kill
 }
 function installlogstash(){
-    log_tarpath='/usr/local/logstash-1.5.6.tar.gz'
+    log_tarpath='/usr/local/package/logstash-1.5.6.tar.gz'
     desfilepath='/usr/local/'
     tar -zvxf ${log_tarpath} -C ${desfilepath}
 }
 function updatelogstash(){
     ipaddr=$1
-   ls_path='/usr/local/beatsfinal.conf'
+   ls_path='/usr/local/package/beatsfinal.conf'
    cp ${ls_path}  /usr/local/logstash-1.5.6/
-   cd /usr/local/logstash-1.5.6/
-   FILE=./beatsfinal.conf
+   FILE=/usr/local/logstash-1.5.6/beatsfinal.conf
    KEY="0.0.0.0"
    sed -i  's/'"$KEY"'/'"${ipaddr}"'/g'  $FILE
 }
-function startlogstash(){ 
-   cd    /usr/local
-   ./logstash-1.5.6/bin/logstash -f ./beatsfinal.conf &
+function startlogstash(){
+    /usr/local/logstash-1.5.6/bin/logstash -f ./beatsfinal.conf &
 }
 function stoplogstash(){
     ps -ef | grep logstash | awk '{print $2}'|xargs kill
 }
 function installkibana(){
-    kb_tarpath='/usr/local/kibana-4.0.3-linux-x64.tar.gz'
+    kb_tarpath='/usr/local/package/kibana-4.0.3-linux-x64.tar.gz'
     desfilepath='/usr/local/'
     tar -zvxf ${kb_tarpath} -C ${desfilepath}  
 }
 function updatekibana(){
     ipaddr=$1
-   cd /usr/local/kibana-4.0.3-linux-x64/config/
-   FILE=./kibana.yml
+   FILE=/usr/local/kibana-4.0.3-linux-x64/config/kibana.yml
    KEY1="0.0.0.0"
    KEY2="localhost:"
    sed -i  's/'"$KEY1"'/'"$ipaddr"'/g'  $FILE
    sed -i  's/'"$KEY2"'/'"$ipaddr":'/g'  $FILE
 }
 function startkibana(){
-   cd /usr/local/
-   ./kibana-4.0.3-linux-x64/bin/kibana &
+    /usr/local/kibana-4.0.3-linux-x64/bin/kibana &
 }
 function stopkibana(){
     ps -ef | grep kibana | awk '{print $2}'|xargs kill
@@ -281,12 +264,13 @@ function changeHostIP(){
     IP=$2
     echo "要修改的网卡配置文件为："
     echo ${eth}
-    ehco "获取内容："
+    echo "获取内容："
     cat ${eth}
     sed -i "13c IPADDR=${IP}" ${eth}
     systemctl restart  network.service
     result=`echo $?`
-    if [ ${result} == "1" ]
+    echo ${result}
+    if [ ${result} == "0" ]
     then
         echo "设置IP成功"
         echo "修改后的文件内容为："
@@ -414,9 +398,9 @@ function testInit(){
     echo "当前节点的角色为：${HostRole}"
 }
 function initEnv(){
-    rolefile="/home/wentao/shell/installHadoopCluster/role.ini"
-    iplistfile="/home/wentao/shell/installHadoopCluster/iplist.ini"
-    tmpfile="/home/wentao/shell/dest/tmp.ini"
+    tmpfile="/root/installHadoopCluster/tmp.ini"
+    rolefile="/root/installHadoopCluster/role.ini"
+    iplistfile="/root/installHadoopCluster/iplist.ini"
     echo "确认当前role.ini路径 : ${rolefile}"
     echo "确认当前iplist.ini路径 : ${iplistfile}"
     normalizdinifile ${rolefile} ${iplistfile} ${tmpfile}
@@ -441,15 +425,12 @@ function initEnv(){
 }
 function install(){
     initEnv
-    ifcfg_file='/home/wentao/shell/dest/ifcfg-ens33'
-    hostsfile='/home/wentao/shell/dest/hosts'
-    ntp_config_file="/home/wentao/shell/dest/ntp.conf"
-    tarfilepath="/home/wentao/shell/package/jdk-8u112-linux-x64.tar.gz"
-    tarfoldername="jdk1.8.0_112"
-    destfilepath="/home/wentao/shell/dest/"
-    destfolderename="JDK"
-    sysintfile="/home/wentao/shell/dest/profile"
-    kafka_config='/home/wentao/shell/dest/server.properties'
+    ifcfg_file='/etc/sysconfig/network-scripts/ifcfg-ens18'
+    hostsfile='/etc/hosts'
+    ntp_config_file="/etc/ntp.conf"
+    ssh_user='root'
+    sysintfile="/etc/profile"
+    kafka_config='/usr/local/hadoop/kafka_2.11-0.9.0.1/config/server.properties'
     mysql_tarfilepath='/usr/local/package/mysql-5.7.6-m16-linux-glibc2.5-x86_64.tar.gz'
     mysql_desfilepath='/usr/local/'
     mysql_filename='mysql-5.7.6-m16-linux-glibc2.5-x86_64'
@@ -470,21 +451,27 @@ function install(){
     is_MySQL=$(substr ${HostRole} "mysql")
     if [ ${is_MySQL} == "1" ]
     then
-        echo "执行MySQL节点安装"
+        echo "开始执行MySQL节点安装"
         installMySQL ${mysql_tarfilepath} ${mysql_desfilepath} ${mysql_filename} ${mysql_cnfpath}
+        echo "执行MySQL节点安装成功"
     else
-        echo "执行MySQL节点安装"
+        echo "不需要执行MySQL节点安装"
     fi
     is_HDFS_master=$(substr ${HostRole} "hdfs_master")
     is_HDFS_salve=$(substr ${HostRole} "hdfs_slave")
     if [[ ${is_HDFS_master} == "1" || ${is_HDFS_salve} == "1" ]]
     then
-        echo "执行HDFS安装部署"
+        echo "开始执行HDFS安装部署"
         hdfs_master_node=${RoleListDict["hdfs_master"]}
         hdfs_second_master_node=${RoleListDict["hdfs_master"]}
         hdfs_slave_node=${RoleListDict["hdfs_slave"]}
         hdfs_slave_nodes=$(replaceBlank2Comma ${hdfs_slave_node})
+        echo ${hdfs_master_node}
+        echo ${hdfs_second_master_node}
+        echo ${hdfs_slave_node}
+        echo ${hdfs_slave_nodes}
         sh hadoop-install.sh -m ${hdfs_master_node} -n ${hdfs_second_master_node} -s ${hdfs_slave_nodes}
+        echo "执行HDFS安装部署成功"
     else
         echo "不需要执行HDFS安装部署"
     fi
@@ -492,65 +479,72 @@ function install(){
     is_HBase_salve=$(substr ${HostRole} "hbase_slave")
     if [[ ${is_HBase_master} == "1" || ${is_HBase_salve} == "1" ]]
     then
-        echo "执行HBase安装部署"
+        echo "开始执行HBase安装部署"
         zookeeper_node=${RoleListDict["zookeeper"]}
         hbase_master_node=${RoleListDict["hbase_master"]}
         hbase_slave_node=${RoleListDict["hbase_slave"]}
         hbase_nodes=$(replaceBlank2Comma ${hbase_slave_node})
         zookeeper_nodes=$(replaceBlank2Comma ${zookeeper_node})
         sh hbase-install.sh -m ${hbase_master_node} -s ${hbase_nodes} -z ${zookeeper_nodes}
+        echo "执行HBase安装部署成功"
     else
         echo "不需要执行HBase安装部署"
     fi
     is_zookeeper=$(substr ${HostRole} "zookeeper")
     if [ ${is_zookeeper} == "1" ]
     then
-        echo "执行zookeeper安装部署"
+        echo "开始执行zookeeper安装部署"
         sh zookeeper-install.sh -q ${zookeeper_nodes}
+        echo "执行zookeeper安装部署成功"
     else
         echo "执行zookeeper安装部署"
     fi
     is_Kafka=$(substr ${HostRole} "kafka")
     if [ ${is_Kafka} == "1" ]
     then
-        echo "执行kafka安装部署"
+        echo "开始执行kafka安装部署"
         kafka_nodes=${RoleListDict["kafka"]}
         setup_kafka "${zookeeper_node}" ${kafka_config}
+        echo "执行kafka安装部署成功"
     else
         echo "不需要执行kafka安装部署"
     fi
     is_es=$(substr ${HostRole} "es")
     if [ ${is_es} == "1" ]
     then
-        echo "执行ELK安装部署"
+        echo "开始执行ES安装部署"
         installES ${HostName}
+        echo "执行ES安装部署成功"
     else
-        echo "不需要执行ELK安装部署"
+        echo "不需要执行ES安装部署"
     fi
     is_filebeat=$(substr ${HostRole} "filebeat")
     if [ ${is_filebeat} == "1" ]
     then
-        echo "执行filebeat安装部署"
+        echo "开始执行filebeat安装部署"
         installfilebeat
         updatefilebeat ${HostName} ${IP}
+        echo "执行filebeat安装部署成功"
     else
         echo "不需要执行filebeat安装部署"
     fi
     is_logstash=$(substr ${HostRole} "logstash")
     if [ ${is_logstash} == "1" ]
     then
-        echo "执行logstash安装部署"
+        echo "开始执行logstash安装部署"
         installlogstash
         updatelogstash ${IP}
+        echo "执行logstash安装部署成功"
     else
         echo "不需要执行logstash安装部署"
     fi
     is_kibana=$(substr ${HostRole} "kibana")
     if [ ${is_kibana} == "1" ]
     then
-        echo "执行kibana安装部署"
+        echo "开始执行kibana安装部署"
         installkibana
         updatekibana ${IP}
+        echo "执行kibana安装部署成功"
     else
         echo "不需要执行kibana安装部署"
     fi
@@ -579,72 +573,81 @@ function startup(){
     is_MySQL=$(substr ${HostRole} "mysql")
     if [ ${is_MySQL} == "1" ]
     then
-        echo "执行MySQL节点服务启动"
+        echo "开始执行MySQL节点服务启动"
         startMySQL
+        echo "执行MySQL节点服务启动成功"
     else
         echo "不需要执行MySQL节点服务启动"
     fi
     is_zookeeper=$(substr ${HostRole} "zookeeper")
     if [ ${is_zookeeper} == "1" ]
     then
-        echo "执行zookeeper节点服务启动"
+        echo "开始执行zookeeper节点服务启动"
         /usr/local/hadoop/zookeeper-3.4.6/bin/zkServer.sh start
+        echo "执行zookeeper节点服务启动成功"
     else
         echo "不需要执行zookeeper节点服务启动"
     fi
     is_HDFS_master=$(substr ${HostRole} "hdfs_master")
     if [ ${is_HDFS_master} == "1" ]
     then
-        echo "执行HDFS主节点服务启动"
+        echo "开始执行HDFS主节点服务启动"
         /usr/local/hadoop/hadoop-2.6.0/sbin/start-dfs.sh
         /usr/local/hadoop/hadoop-2.6.0/sbin/start-yarn.sh
+        echo "执行HDFS主节点服务启动成功"
     else
         echo "不需要执行HDFS主节点服务启动"
     fi
     is_HBase_master=$(substr ${HostRole} "hbase_master")
     if [ ${is_HBase_master} == "1" ]
     then
-        echo "执行HBase主节点服务启动"
+        echo "开始执行HBase主节点服务启动"
         /usr/local/hadoop/hbase-1.2.0/bin/start-hbase.sh
+        echo "执行HBase主节点服务启动成功"
     else
         echo "不需要执行HBase主节点服务启动"
     fi
     is_Kafka=$(substr ${HostRole} "kafka")
     if [ ${is_Kafka} == "1" ]
     then
-        echo "执行kafka节点服务启动"
+        echo "开始执行kafka节点服务启动"
+        echo "执行kafka节点服务启动成功"
     else
         echo "不需要执行kafka节点服务启动"
     fi
     is_es=$(substr ${HostRole} "es")
     if [ ${is_es} == "1" ]
     then
-        echo "执行ES节点服务启动"
+        echo "开始执行ES节点服务启动"
         startES
+        echo "执行ES节点服务启动成功"
     else
         echo "不需要执行ES节点服务启动"
     fi
     is_filebeat=$(substr ${HostRole} "filebeat")
     if [ ${is_filebeat} == "1" ]
     then
-        echo "执行filebeat节点服务启动"
+        echo "开始执行filebeat节点服务启动"
         startfilebeat
+        echo "执行filebeat节点服务启动成功"
     else
         echo "不需要执行filebeat节点服务启动"
     fi
     is_logstash=$(substr ${HostRole} "logstash")
     if [ ${is_logstash} == "1" ]
     then
-        echo "执行logstash节点服务启动"
+        echo "开始执行logstash节点服务启动"
         startlogstash
+        echo "执行logstash节点服务启动成功"
     else
         echo "不需要执行logstash节点服务启动"
     fi
     is_kibana=$(substr ${HostRole} "kibana")
     if [ ${is_kibana} == "1" ]
     then
-        echo "执行kibana节点服务启动"
+        echo "开始执行kibana节点服务启动"
         startkibana
+        echo "执行kibana节点服务启动成功"
     else
         echo "不需要执行kibana节点服务启动"
     fi
@@ -661,72 +664,81 @@ function stop(){
     is_Kafka=$(substr ${HostRole} "kafka")
     if [ ${is_Kafka} == "1" ]
     then
-        echo "执行kafka节点关闭"
+        echo "开始执行kafka节点关闭"
+        echo "执行kafka节点关闭成功"
     else
         echo "不需要执行kafka节点启动"
     fi
     is_HBase_master=$(substr ${HostRole} "hbase_master")
     if [ ${is_HBase_master} == "1" ]
     then
-        echo "执行HBase主节点关闭"
+        echo "开始执行HBase主节点关闭"
         /usr/local/hadoop/hbase-1.2.0/bin/stop-hbase.sh
+        echo "执行HBase主节点关闭成功"
     else
         echo "不需要执行HBase主节点关闭"
     fi
     is_HDFS_master=$(substr ${HostRole} "hdfs_master")
     if [ ${is_HDFS_master} == "1" ]
     then
-        echo "执行HDFS主节点关闭"
+        echo "开始执行HDFS主节点关闭"
         /usr/local/hadoop/hadoop-2.6.0/sbin/stop-dfs.sh
         /usr/local/hadoop/hadoop-2.6.0/sbin/stop-yarn.sh
+        echo "执行HDFS主节点关闭成功"
     else
         echo "不需要执行HDFS主节点关闭"
     fi
     is_zookeeper=$(substr ${HostRole} "zookeeper")
     if [ ${is_zookeeper} == "1" ]
     then
-        echo "执行zookeeper节点服务关闭"
+        echo "开始执行zookeeper节点服务关闭"
         /usr/local/hadoop/zookeeper-3.4.6/bin/zkServer.sh stop
+        echo "执行zookeeper节点服务关闭成功"
     else
         echo "不需要执行zookeeper节点关闭"
     fi
     is_MySQL=$(substr ${HostRole} "mysql")
     if [ ${is_MySQL} == "1" ]
     then
-        echo "执行MySQL节点服务关闭"
+        echo "开始执行MySQL节点服务关闭"
         stopMySQL
+        echo "执行MySQL节点服务关闭成功"
     else
         echo "不需要执行MySQL节点服务关闭"
     fi
     is_es=$(substr ${HostRole} "es")
     if [ ${is_es} == "1" ]
     then
-        echo "执行ES节点服务关闭"
+        echo "开始执行ES节点服务关闭"
         stopES
+        echo "执行ES节点服务关闭成功"
     else
         echo "不需要执行ES节点服务关闭"
     fi
     is_filebeat=$(substr ${HostRole} "filebeat")
     if [ ${is_filebeat} == "1" ]
     then
-        echo "执行filebeat节点服务关闭"
+        echo "开始执行filebeat节点服务关闭"
         stopfilebeat
+        echo "执行filebeat节点服务关闭成功"
     else
         echo "不需要执行filebeat节点服务关闭"
     fi
     is_logstash=$(substr ${HostRole} "logstash")
     if [ ${is_logstash} == "1" ]
     then
-        echo "执行logstash节点服务关闭"
+        echo "开始执行logstash节点服务关闭"
         stoplogstash
+        echo "执行logstash节点服务关闭成功"
     else
         echo "不需要执行logstash节点服务关闭"
     fi
     is_kibana=$(substr ${HostRole} "kibana")
     if [ ${is_kibana} == "1" ]
     then
-        echo "执行kibana节点服务关闭"
+        echo "开始执行kibana节点服务关闭"
         stopkibana
+        echo "执行kibana节点服务关闭成功"
     else
         echo "不需要执行kibana节点服务关闭"
     fi
